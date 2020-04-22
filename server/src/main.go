c8e9443b6e3564fc0aad0c120cfbdce5295426ae
Sansossio <juliosansossio@gmail.com>
2020-04-22 13:53:35 +0200

    add cors

--- server/src/main.go
+++ server/src/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	socketio "github.com/googollee/go-socket.io"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -14,23 +15,8 @@ func main() {
 		log.Fatal(err)
 	}
 	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
 		fmt.Println("connected:", s.ID())
 		return nil
-	})
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
 	})
 	server.OnError("/", func(s socketio.Conn, e error) {
 		fmt.Println("meet error:", e)
@@ -43,6 +29,11 @@ func main() {
 
 	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("../asset")))
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"https://postwoman.io"},
+		AllowCredentials: true,
+	})
 	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", c.Handler(server)))
 }
