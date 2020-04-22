59cae45896036df64677daa49d262bed76d16935
Sansossio <juliosansossio@gmail.com>
2020-04-22 14:03:28 +0200

    Remove cors package

--- server/src/main.go
+++ server/src/main.go
@@ -2,38 +2,40 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	socketio "github.com/googollee/go-socket.io"
-	"github.com/rs/cors"
 )
 
 func main() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("socket new server err: %v\n", err)
+		return
 	}
-	server.OnConnect("/", func(s socketio.Conn) error {
-		fmt.Println("connected:", s.ID())
+
+	server.OnConnect("/", func(conn socketio.Conn) error {
+		conn.SetContext("")
+		fmt.Printf("connected: %v\n", conn.ID())
 		return nil
 	})
+
 	server.OnError("/", func(s socketio.Conn, e error) {
 		fmt.Println("meet error:", e)
 	})
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
+
+	server.OnDisconnect("/", func(conn socketio.Conn, msg string) {
+		fmt.Printf("closed, msg: %v\n", msg)
 	})
+
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
-	http.Handle("/", http.FileServer(http.Dir("../asset")))
-
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://postwoman.io"},
-		AllowCredentials: true,
+	http.HandleFunc("/socket.io/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		server.ServeHTTP(w, r)
 	})
-	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", c.Handler(server)))
+	fmt.Println(http.ListenAndServe(":8000", nil))
 }
