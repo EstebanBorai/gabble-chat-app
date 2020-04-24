3868ef0a3099ceff6bd68d496ef576bf6e69d126
Sansossio <juliosansossio@gmail.com>
2020-04-24 18:22:20 +0200

    Update primitive events with config

--- server/src/chat/events/primitive_events.go
+++ server/src/chat/events/primitive_events.go
@@ -4,23 +4,24 @@ import (
 	"log"
 
 	socketio "github.com/googollee/go-socket.io"
+	config "github.com/whizzes/gabble/server/src/chat/config"
 )
 
 // InitBasicEvents Listening primitives events
 func InitBasicEvents(server *socketio.Server) {
-	server.OnConnect("/", func(so socketio.Conn) error {
+	server.OnConnect(config.PATH, func(so socketio.Conn) error {
 		so.SetContext("")
 		log.Printf("Connection Opened with ID: %s", so.ID())
 
 		return nil
 	})
 
-	server.OnError("/", func(conn socketio.Conn, e error) {
+	server.OnError(config.PATH, func(conn socketio.Conn, e error) {
 		// print the error message and stop
 		log.Fatal(e)
 	})
 
-	server.OnDisconnect("/", func(conn socketio.Conn, reason string) {
+	server.OnDisconnect(config.PATH, func(conn socketio.Conn, reason string) {
 		log.Println("closed", reason)
 	})
 }
