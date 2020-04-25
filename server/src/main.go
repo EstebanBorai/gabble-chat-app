1cd21d94a20e02b11d7767e1930f708d57227da9
Sansossio <juliosansossio@gmail.com>
2020-04-25 14:16:19 +0200

    Go dotenv implementation

--- server/src/main.go
+++ server/src/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/whizzes/gabble/server/src/chat"
 )
 
 func main() {
-	server, err := chat.MakeServer(":8000")
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	server, err := chat.MakeServer(os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))
 
 	if err != nil {
 		fmt.Printf("socket new server err: %v\n", err)
