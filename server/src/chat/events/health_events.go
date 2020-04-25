286a26ef1ef8d252f2c5d94f79639927ab592fda
Sansossio <juliosansossio@gmail.com>
2020-04-25 14:29:08 +0200

    Remove log

--- server/src/chat/events/health_events.go
+++ server/src/chat/events/health_events.go
@@ -1,8 +1,6 @@
 package chat
 
 import (
-	"fmt"
-
 	socketio "github.com/googollee/go-socket.io"
 	config "github.com/whizzes/gabble/server/src/chat/config"
 )
@@ -14,7 +12,6 @@ const (
 
 // InitHealtEvents Basic health events
 func InitHealtEvents(server *socketio.Server) {
-	fmt.Println(PingEvent)
 	server.OnEvent(config.PATH, PingEvent, func(so socketio.Conn, msg string) {
 		so.Emit(PingEvent, msg)
 	})
